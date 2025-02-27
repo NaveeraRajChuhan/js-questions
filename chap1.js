785742d0de383ccc3150e53842cf6b94eb858ed6
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-27 19:35:07 +0500

    question 4 done

---
+++ chap1.js
@@ -0,0 +1,57 @@
+// // question link
+// file:///C:/Users/DELL/Downloads/chapter1%20(1).pdf
+// _____________________________________________________________________________________________________
+
+
+// ------------------------------------------------------CHAP NO 1----------------------------
+// __________QUESTION 1________________
+// 1. Write a script to greet your website visitor using JS alert box
+
+
+
+
+
+
+
+
+// // __________QUESTION 2________________
+// // 2. Write a script to display following message on your web page 
+// var pasword ="Error! please  enter a valid pasword";
+// alert(pasword)
+
+
+
+// // __________QUESTION 3________________
+// // 3.  Write a script to display following message on your web page: (Hint : Use line break)
+// var welcome ="Welcome to JS land... \n Happy Coding";
+// alert(welcome)
+
+
+
+// // __________QUESTION 4________________
+// // 4. Write a script to display following messages in sequence:
+// var welcome ="Welcome to JS land...";
+// alert(welcome)
+
+// var welcome ="Prevent this pagefrom creating additional dialogs";
+// alert(welcome)
+
+
+
+
+// __________QUESTION 5________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
