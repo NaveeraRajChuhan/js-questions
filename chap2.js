496726dc1181fd7cacf1d27af447dd8446ed9669
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-27 22:23:56 +0500

    chap2 question8

--- chap2.js
+++ chap2.js
@@ -32,4 +32,8 @@
 
 
 
+// // __________QUESTION 8________________
+// // 8. Write a script to display this in browser through JS
+// document.write("<h1> Yah! I can write HTML content through JavaScript </h1>")
+
 
