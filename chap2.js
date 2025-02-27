4aa8d64cfc6bb8d6833582a3f8cdc70212c34984
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-27 19:56:29 +0500

    chap 2 ques 1

---
+++ chap2.js
@@ -0,0 +1,13 @@
+// // question link
+// file:///C:/Users/DELL/Downloads/chapter2.pdf
+// _____________________________________________________________________________________________________
+
+
+// // ------------------------------------------------------CHAP NO 2----------------------------
+// // __________QUESTION 1________________
+// // 1. Declare a variable called username.
+// var username ="Jhone Doe";
+//  alert(username)
+
+
+
