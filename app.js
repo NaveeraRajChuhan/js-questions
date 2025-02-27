4f46f69227d0558b53d75666c1ffd172e12f97ab
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-27 19:02:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// ------------------------------------------------------CHAP NO 1----------------------------
+// __________QUESTION 1________________
+// Write a script to greet your website visitor using JS alert box
+
+
+
+
+
+
+
+
+// __________QUESTION 2________________
+// Write a script to display following message on your web page 
+
