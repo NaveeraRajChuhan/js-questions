4747a6e51f2ba58ff83bc1743d1b166530249b33
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-28 13:04:48 +0500

    chap 3 start

---
+++ chap3.js
@@ -0,0 +1,8 @@
+// // question link
+// file:///C:/Users/DELL/Downloads/chapter3.pdf
+// _____________________________________________________________________________________________________
+
+
+// ------------------------------------------------------CHAP NO 3----------------------------
+// __________QUESTION 1________________
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
